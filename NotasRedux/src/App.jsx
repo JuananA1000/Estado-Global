8282b37c0e8943f3cd2afdb363fbc9b1021760fa
Juanan <juanantonioamillopez@gmail.com>
2024-05-16 12:47:44 +0200

    GUARDAR

--- NotasRedux/src/App.jsx
+++ NotasRedux/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 
+import Button from './components/Button/Button';
+
 function App() {
   return (
     <div>
       <h1>Gestor de Notas</h1>
       <div className='card'>{/* Aquí el código */}</div>
+      <Button>Añadir Nota</Button>
     </div>
   );
 }
