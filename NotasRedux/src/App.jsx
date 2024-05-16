0439ad3f5b1a2831973d5104c7ba3f4d55eb1a79
Juanan <juanantonioamillopez@gmail.com>
2024-05-16 09:17:44 +0200

    Inicializar proyecto: Gestor de Notas con Redux

---
+++ NotasRedux/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Gestor de Notas</h1>
+      <div className='card'>{/* Aquí el código */}</div>
+    </div>
+  );
+}
+
+export default App;
