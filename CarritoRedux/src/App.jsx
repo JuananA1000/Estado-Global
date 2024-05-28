bcfc5cd80f904fafdcbac3a8c6c31aa24d5169f7
Juanan <juanantonioamillopez@gmail.com>
2024-05-28 12:09:35 +0200

    Nuevo proyecto: Carrito de compras

---
+++ CarritoRedux/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  
+  return (
+    <div>
+      <h1>Carrito de compras</h1>
+      <div className='card'>
+        {/* 
+          Aquí el código
+        */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
