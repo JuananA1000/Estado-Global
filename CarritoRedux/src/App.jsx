9e7b693209f8e113bc9686a2f8a767974759dcf8
Juanan <juanantonioamillopez@gmail.com>
2024-05-28 12:55:19 +0200

    Escaparate de productos

--- CarritoRedux/src/App.jsx
+++ CarritoRedux/src/App.jsx
@@ -1,14 +1,20 @@
+import instrumentos from './data/instrumentosData.js';
+
 import './App.css';
 
 function App() {
-  
   return (
     <div>
       <h1>Carrito de compras</h1>
       <div className='card'>
-        {/* 
-          Aquí el código
-        */}
+        {instrumentos.map((instrumento, index) => (
+          <div className='producto' key={index}>
+            <img src={instrumento.img} alt={instrumento.nombre} width={150} />
+            <p>{instrumento.nombre}</p>
+            <p>{instrumento.precio} €</p>
+            <button>Añadir</button>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ CarritoRedux/src/data/instrumentosData.js
@@ -0,0 +1,29 @@
+import baquetas from '../img/baquetas.jpg'
+import bateria from '../img/bateria.jpg'
+import caja from '../img/caja.jpg'
+import plato from '../img/plato.jpg'
+
+const instrumentos = [
+  {
+    nombre: 'Wincent 5A Hickory',
+    precio: 12.4,
+    img: baquetas,
+  },
+  {
+    nombre: 'Sonor AQX Blue',
+    precio: 999,
+    img: bateria,
+  },
+  {
+    nombre: 'Sonor Benny Greb Signature',
+    precio: 879,
+    img: caja,
+  },
+  {
+    nombre: 'Paiste 14" PSTX Swiss Thin Crash',
+    precio: 85,
+    img: plato,
+  },
+];
+
+export default instrumentos;
