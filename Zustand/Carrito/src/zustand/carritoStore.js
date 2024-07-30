c9465e7e994d1922e87280c1e93efe0143009161
Juanan <juanantonioamillopez@gmail.com>
2024-07-30 13:41:41 +0200

    Estructura zustand

---
+++ Zustand/Carrito/src/zustand/carritoStore.js
@@ -0,0 +1,12 @@
+import { create } from 'zustand';
+
+export const carritoStore = create((set) => ({
+  carrito: [],
+
+  addInstrumento: () => {
+    // code
+  },
+  eliminarInstrumento: () => {
+    // code
+  },
+}));
