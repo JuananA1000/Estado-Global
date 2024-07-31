9dcd950a6cbd0d33230b94b883636858614916e4
Juanan <juanantonioamillopez@gmail.com>
2024-07-31 12:51:51 +0200

    plantilla zustand con funciones indicadas

--- Zustand/Carrito/src/zustand/carritoStore.js
+++ Zustand/Carrito/src/zustand/carritoStore.js
@@ -1,11 +1,16 @@
 import { create } from 'zustand';
 
 export const carritoStore = create((set) => ({
-  carrito: [],
-
-  addInstrumento: () => {
-    // code
+  carrito: {
+    items: [],
+    total: 0,
   },
+
+  addInstrumento: (instrumento) =>
+    set((estado) => ({
+      // code
+    })),
+
   eliminarInstrumento: () => {
     // code
   },
