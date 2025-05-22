6a9907ee1dd89712afcff1d57d38a0352cd99dfa
Juanan <juanantonioamillopez@gmail.com>
2025-05-22 13:26:52 +0200

    FEAT: Fetching genérico

--- Redux/Meteo/src/App.jsx
+++ Redux/Meteo/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState, useEffect } from 'react';
+import useFetch from './hooks/useFetch';
 
-import './App.css'
+import './App.css';
 
 function App() {
+  const { data, loading, error } = useFetch(
+    'https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m&current_weather=true'
+  );
+
+  console.log(data);
 
   return (
     <div>
       <h1>Meteo</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
