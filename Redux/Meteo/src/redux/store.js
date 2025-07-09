0f3dab2b12c31772d78756b55b7cbeb1a8edc3b9
Juanan <juanantonioamillopez@gmail.com>
2025-07-09 12:41:49 +0200

    FEAT: crear slice para ubicacion

--- Redux/Meteo/src/redux/store.js
+++ Redux/Meteo/src/redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import meteoSlice from './meteoSlice';
+import locationSlice from './locationSlice';
 
 export const store = configureStore({
   reducer: {
     location: locationSlice,
     meteo: meteoSlice,
-
   },
 });
