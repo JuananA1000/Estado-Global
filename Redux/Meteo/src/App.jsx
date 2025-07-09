1db7c467eff4c642eeda602764d40ca8fedf5b0b
Juanan <juanantonioamillopez@gmail.com>
2025-07-09 10:50:53 +0200

    FEAT: Estilo Básico

--- Redux/Meteo/src/App.jsx
+++ Redux/Meteo/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import useFetch from './hooks/useFetch';
 
+
+
 import './App.css';
+import wind from './assets/wind.png';
 
 function App() {
   const { data, loading, error } = useFetch(
@@ -13,6 +16,22 @@ function App() {
   return (
     <div>
       <h1>Meteo</h1>
+
+      <div className="container">
+        {loading && <p>Cargando...</p>}
+        {error && <p>Error al obtener los datos</p>}
+        {data && (
+          <div>
+
+            <input type="text" placeholder="Buscar ciudad..." className='search-input' />
+            <img src={wind} alt="Weather Icon" width={100} />
+
+            <h2>El tiempo en nombreCiudad</h2>
+            <p>Temperatura: {data.current_weather.temperature}°C</p>
+            <p>Velocidad del Viento: {data.current_weather.windspeed} km/h</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
