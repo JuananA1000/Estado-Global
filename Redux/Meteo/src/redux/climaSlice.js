ecc09adbcad08a3c90ce29f7c8a2ec45745e4a5d
Juanan <juanantonioamillopez@gmail.com>
2025-07-10 12:09:12 +0200

    FIX: Modificar climaSlice

--- Redux/Meteo/src/redux/climaSlice.js
+++ Redux/Meteo/src/redux/climaSlice.js
@@ -1,19 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Utilizamos createAsyncThunk de Redux Toolkit para  acciones asíncronas
+const fetchClima = createAsyncThunk('clima/fetchClima', async ({ latitud, longitud }) => {
+  const response = await axios.get(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&current_weather=true`
+  );
+  return response.data.current_weather;
+});
 
 const climaSlice = createSlice({
   name: 'clima',
-  
+
   initialState: {
     data: null,
-    loading: false,
-    error: null,
+    status: 'idle', // 'idle', 'loading', 'succeeded', 'failed'
   },
 
-  reducers: {
-    // 1. Actualizar ubicación
-    // 2. Cargar datos del clima
+  reducers: {},
+
+  // extraReducers se usa cuando reaccionamos a acciones externas al slice, como las que genera createAsyncThunk.
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchClima.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchClima.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.data = action.payload;
+      })
+      .addCase(fetchClima.rejected, (state) => {
+        state.status = 'failed';
+      });
   },
 });
 
-export const {  } = climaSlice.actions;
 export default climaSlice.reducer;
