e2ce58e03ab5bc18850758305ec7326f0e2b5b3b
Juanan <juanantonioamillopez@gmail.com>
2025-07-11 14:56:08 +0200

    FEAT: Preparar plantilla Redux

---
+++ Redux/Meteo/src/redux/climaSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const climaSlice = createSlice({
+  name: 'clima',
+  initialState: {},
+  reducers: {},
+});
+
+export const {} = climaSlice.actions;
+export default climaSlice.reducer;
