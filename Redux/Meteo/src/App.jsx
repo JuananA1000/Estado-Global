d8895278e76ca2e0ac9f90226ceba2b9bd284672
Juanan <juanantonioamillopez@gmail.com>
2025-07-14 09:45:12 +0200

    FEAT: Estructura básica

--- Redux/Meteo/src/App.jsx
+++ Redux/Meteo/src/App.jsx
@@ -4,6 +4,15 @@ function App() {
   return (
     <>
       <h1>Meteo</h1>
+      
+      <input type="text" placeholder="Buscar Ciudad..." />
+      <button>Buscar</button>
+
+      <div className="card">
+        <h2>Nombre Ciudad</h2>
+        <p>Condición: Soleado</p>
+        <p>Temperatura: 25°C</p>
+      </div>
     </>
   );
 }
