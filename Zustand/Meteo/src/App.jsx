519dac80cd9063a7fcf6eabd543650ebe87656e4
Juanan <juanantonioamillopez@gmail.com>
2025-08-18 13:53:53 +0200

    FEAT:  Estilo Básico

--- Zustand/Meteo/src/App.jsx
+++ Zustand/Meteo/src/App.jsx
@@ -1,9 +1,23 @@
+import sun from './assets/0,1.png';
+
 import './App.css';
 
 function App() {
   return (
     <>
       <h1>Meteo</h1>
+
+      <div className='container'>
+        <input type='text' placeholder='Buscar Ciudad...' />
+        <button>Buscar</button>
+
+        <div className='card'>
+          <h2>nombreCiudad</h2>
+          <img src={sun} alt='Weather Icon' width={50} />
+
+          <h3>00°C</h3>
+        </div>
+      </div>
     </>
   );
 }
