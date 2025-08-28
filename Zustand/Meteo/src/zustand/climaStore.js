836dcd3de5fd9d88178fb5f778d5e3d8c21ebf29
Juanan <juanantonioamillopez@gmail.com>
2025-08-28 12:12:47 +0200

    FIX: Borrar comentarios

--- Zustand/Meteo/src/zustand/climaStore.js
+++ Zustand/Meteo/src/zustand/climaStore.js
@@ -1,9 +1,5 @@
 import { create } from 'zustand';
 
-// Pillar ciudades del linkhttps://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(nomCiudad)}
-// Y condiciones climáticas de aquí: https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true
-// DUDA: ¿Es necesario crear dos stores, o puedo uno solo?
-
 const climaStore = create((set) => ({
   data: null,
   loading: false,
