192c140df243981c81be7812a2646d6dbcc71dfc
Juanan <juanantonioamillopez@gmail.com>
2026-02-19 12:58:22 +0100

    FEAT: obtener clima con coords hardcodeadas

--- Zustand/Meteo/src/App.jsx
+++ Zustand/Meteo/src/App.jsx
@@ -1,39 +1,45 @@
 import { useEffect, useState } from 'react';
 
 import { climaStore } from './zustand/climaStore';
+import { ubicacionStore } from './zustand/ubicacionStore';
 
 import './App.css';
 
 function App() {
   const [ciudad, setCiudad] = useState('');
 
+  const { obtenerUbicacion } = ubicacionStore();
   const { clima, setDatosClima } = climaStore();
 
-  const buscarCiudad = () => {
-    console.log('Buscar ciudad no implementado');
+  const buscarCiudad = async () => {
+    await obtenerUbicacion(ciudad);
+    const { lat, lon } = ubicacionStore.getState();
+    await setDatosClima(40.4167, -3.7033); // Coordenadas de Madrid
+
+    console.log(clima);
   };
 
-  // useEffect(() => {
-  //   setDatosClima(40.4168, -3.7038); // Coordenadas de Madrid
-  // }, [setDatosClima]);
+  // Coordenadas MAD: 40.4167, -3.7033
 
   return (
     <>
       <h1>Meteo</h1>
 
       <div className='container'>
-        <input type='text' placeholder='Buscar Ciudad...' />
+        <input type='text' placeholder='Buscar Ciudad...' value={ciudad} onChange={(e) => setCiudad(e.target.value)} />
         <button onClick={buscarCiudad}>Buscar</button>
 
-        <div className='card'>
-          <h2>nombreCiudad</h2>
-          <img
-            // src={weatherData.find((w) => w.weathercode.includes(data?.weathercode))?.icono}
-            alt='Weather Icon'
-            width={50}
-          />
-          <h3>00°C</h3>
-        </div>
+        {clima && (
+          <div className='card'>
+            <h2>{clima?.nombreCiudad || 'nombreCiudad'}</h2>
+            <img
+              // src={weatherData.find((w) => w.weathercode.includes(data?.weathercode))?.icono}
+              alt='Weather Icon'
+              width={50}
+            />
+            <h3>{clima?.temperature || '00°C'}</h3>
+          </div>
+        )}
       </div>
     </>
   );
--- Zustand/Meteo/src/zustand/climaStore.js
+++ Zustand/Meteo/src/zustand/climaStore.js
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 
-const METEO_API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=';
 // Obtener datos de esta dirección: `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
 export const climaStore = create((set) => ({
   clima: null,
@@ -13,14 +12,14 @@ export const climaStore = create((set) => ({
     set({ loading: true, error: null });
 
     try {
-      const response = await fetch(`${METEO_API_URL}${latitude}&longitude=${longitude}&current_weather=true`);
+      const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
 
       if (!response.ok) {
         throw new Error('Error al obtener los datos del clima');
       }
 
       const data = await response.json();
-      set({ clima: data, loading: false });
+      set({ clima: data.current_weather, loading: false });
     } catch (error) {
       set({ error: error.message, loading: false });
     }
