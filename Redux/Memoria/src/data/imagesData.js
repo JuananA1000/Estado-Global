cdbd5e5da43f44b8866caf90b34bf84bf0637d28
Juanan <juanantonioamillopez@gmail.com>
2026-03-05 13:44:16 +0100

    FEAT: Objeto de imágenes

---
+++ Redux/Memoria/src/data/imagesData.js
@@ -0,0 +1,21 @@
+import mariscada from '../img/mariscada.png';
+import montanna_alien from '../img/montaña_alien.png';
+import oso_panda from '../img/oso_panda.png';
+import scary_movie from '../img/scary_movie.png';
+import serpientes_en_el_avion from '../img/serpientes_en_el_avion.png';
+import sharknado from '../img/sharknado.png';
+import tambor_derretido from '../img/tambor_derretido.png';
+import tortuga_ninja from '../img/tortuga_ninja.png';
+
+const imagesData = [
+  { id: 1, name: 'Mariscada', img: mariscada },
+  { id: 2, name: 'Montaña Alien', img: montanna_alien },
+  { id: 3, name: 'Oso Panda', img: oso_panda },
+  { id: 4, name: 'Scary Movie', img: scary_movie },
+  { id: 5, name: 'Serpientes en el Avión', img: serpientes_en_el_avion },
+  { id: 6, name: 'Sharknado', img: sharknado },
+  { id: 7, name: 'Tambor Derretido', img: tambor_derretido },
+  { id: 8, name: 'Tortuga Ninja', img: tortuga_ninja },
+];
+
+export default imagesData;
