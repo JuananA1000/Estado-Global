be7c1c54dd94b01d1b345f0b64ffe2f36f1014a9
Juanan <juanantonioamillopez@gmail.com>
2026-03-09 14:26:59 +0100

    FEAT: Botón inicio

--- Redux/Memoria/src/App.jsx
+++ Redux/Memoria/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import imagesData from './data/imagesData';
 
 import './App.css';
@@ -5,13 +7,18 @@ import './App.css';
 const imgRandom = [...imagesData, ...imagesData].sort(() => Math.random() - 0.5).map((img, i) => ({ ...img, key: i }));
 
 function App() {
+  const [start, setStart] = useState(false);
+
   return (
     <>
       <h1>Juego de Memoria</h1>
+
+      <button onClick={() => setStart(!start)}>{start ? 'Reiniciar' : 'Iniciar'}</button>
+
       <div className='card'>
         {imgRandom.map(({ key, img, name }) => (
           <div key={key} className='tarjeta'>
-            <img src={img} alt={name} width={80} />
+            {start && <img src={img} alt={name} width={80} />}
           </div>
         ))}
       </div>
