a454156f1f92c5ae0550326ecf023765dc1b4377
Juanan <juanantonioamillopez@gmail.com>
2026-03-17 12:59:44 +0100

    FEAT: Empezamos de cero

--- Redux/Memoria/src/App.jsx
+++ Redux/Memoria/src/App.jsx
@@ -10,34 +10,7 @@ import './App.css';
 const imgRandom = [...imagesData, ...imagesData].sort(() => Math.random() - 0.5).map((img, i) => ({ ...img, key: i }));
 
 function App() {
-  const [cartasGiradas, setCartasGiradas] = useState([]);
-
-  const dispatch = useDispatch();
-
-  const cartas = useSelector((state) => state.memoria.cartas);
-  const cartasSeleccionadas = useSelector((state) => state.memoria.cartasSeleccionadas);
-  const bloquearTablero = useSelector((state) => state.memoria.bloquearTablero);
-
-  const seleccionarCarta = (carta) => {
-    if (bloquearTablero) return;
-    dispatch(selectCarta(carta));
-    setCartasGiradas([...cartasGiradas, carta.id]);
-  };
-
-  useEffect(() => {
-    if (cartasSeleccionadas.length === 2) {
-      setTimeout(() => {
-        dispatch(compararCartas());
-      }, 1000);
-    }
-  }, [cartasSeleccionadas, dispatch]);
-
-  // Cuando se limpian las cartas seleccionadas, también limpiamos las giradas
-  useEffect(() => {
-    if (cartasSeleccionadas.length === 0) {
-      setCartasGiradas([]);
-    }
-  }, [cartasSeleccionadas]);
+ 
 
   return (
     <>
@@ -48,7 +21,7 @@ function App() {
       <div className='card'>
         {imgRandom.map(({ key, img, name }) => (
           <div key={key} className='tarjeta' onClick={() => seleccionarCarta(  { id: key, valor: name })}>
-            {cartasGiradas.includes(key) && <img src={img} alt={name} width={80} />}
+          <img src={img} alt={name} width={80} />
           </div>
         ))}
       </div>
