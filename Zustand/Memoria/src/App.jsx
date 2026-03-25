bd1334346ae8ff0061543d9e4a32df791c45e5ec
Juanan <juanantonioamillopez@gmail.com>
2026-03-25 12:37:16 +0100

    FEAT: Crear carta

--- Zustand/Memoria/src/App.jsx
+++ Zustand/Memoria/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import reves from './img/svg/reves.svg';
+
 import './App.css';
 
 function App() {
@@ -8,6 +10,17 @@ function App() {
   return (
     <>
       <h1>Juego de Memoria</h1>
+
+      <div className='card'>
+        <div className='tarjeta'>
+          <img src={reves} alt='Reves' width={80} style={{ opacity: 0.2 }} />
+        </div>
+      </div>
+
+      <div>
+        <h3></h3>
+        <button>Reiniciar</button>
+      </div>
     </>
   );
 }
